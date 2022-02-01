473743de8aa470be607a16f6b172d55eb11757ff
Kirby Gordon <kirby.gordon@faraday.io>
2022-02-01 14:16:21 -0500

    ready to release with beginnings of formalism

--- barbq/query.py
+++ barbq/query.py
@@ -1,5 +1,4 @@
 from abc import abstractmethod
-from dis import _HaveCodeOrStringType
 import sqlparse
 from typing import List, Optional, Tuple, Union
 from enum import Enum, auto
@@ -24,13 +23,25 @@ RP = Token(")", C.SPECIAL) # right-parenthesis
 COMMA = Token(",", C.SPECIAL)
 
 class SQL:
+    def __init__(self):
+        self._raw = None
+
     def render(self) -> str:
         return sqlparse.format(" ".join([self._delex(token) for token in self._serialize()]))
 
     @classmethod
     def raw(cls, text: str):
-        inst = cls()
-        inst._raw = Token(text, C.RAW)
+        try:
+            inst = cls()
+        except:
+            try:
+                inst = cls("")
+            except:
+                try:
+                    inst = cls(Table.raw(""))
+                except:
+                    inst = cls(FROM=Table.raw(""))
+        inst._raw = text
         return inst
 
     @classmethod
@@ -44,7 +55,8 @@ class SQL:
     def _chain(cls, components: List["SQL"]) -> List[Token]:
         result = []
         for sql in components:
-            result.extend(sql._serialize())
+            if sql:
+                result.extend(sql._serialize())
         return result
 
     @classmethod
@@ -64,15 +76,12 @@ class SQL:
         if token.category == C.LITERAL:
             return f"'{token.text}'"
         if token.category == C.IDENTIFIER:
-            return "".join([f"`{path_component}`" for path_component in token.text.split(".")])
+            return ".".join([f"`{path_component}`" for path_component in token.text.split(".")])
 
-
-    @abstractmethod
-    def __serialize(self) -> List[Token]:
+    def _serialize_(self) -> List[Token]:
         pass
-
     def _serialize(self) -> List[Token]:
-        return [self._raw] if self._raw else self.__serialize()
+        return [Token(self._raw, C.RAW)] if self._raw else self._serialize_()
 
 
 class SetOperation(SQL):
@@ -105,72 +114,83 @@ class On(SQL):
     pass
 class Col(SQL):
     _text: str
+    _alias: Optional[str]
 
-    def __serialize(self) -> List[Token]:
-        return [Token(self._text, C.IDENTIFIER)]
+    # syntactic sugar for changing or setting alias
+    def AS(self, alias: str) -> None:
+        self._alias = alias
+        return self
 
-    def __init__(self, text: str):
+    def _serialize_(self) -> List[Token]:
+        return [Token(self._text, C.IDENTIFIER)] + ([AS, Token(self._alias, C.IDENTIFIER)] if self._alias else [])
+
+    def __init__(self, text: str, AS: Optional[str] = None):
+        super().__init__()
         self._text = text
+        self._alias = AS
 class Table(SQL): # yes, I know this is identical to Col for now
     _text: str
-    def __serialize(self) -> List[Token]:
-        return [Token(self._text, C.IDENTIFIER)]
-    def __init__(self, text: str):
+    _alias: Optional[str]
+
+    # syntactic sugar for changing or setting alias
+    def AS(self, alias: str) -> None:
+        self._alias = alias
+        return self
+
+    def _serialize_(self) -> List[Token]:
+        return [Token(self._text, C.IDENTIFIER)] + ([AS, Token(self._alias, C.IDENTIFIER)] if self._alias else [])
+
+    def __init__(self, text: str, AS: Optional[str] = None):
+        super().__init__()
         self._text = text
+        self._alias = AS
 class Join(SQL):
     _from_item: Union[Table, "Query"]
     _join_condition: Union[On, Using]
 
-    def __serialize(self) -> List[Token]:
+    def _serialize_(self) -> List[Token]:
         return [Token("JOIN", C.KEYWORD)] + self._table_or_query(self._from_item) + self._join_condition._serialize()
 
     def __init__(self, args: Tuple[Union[Table, "Query"], Union[On, Using]]):
+        super().__init__()
         self._from_item, self._join_condition = args
 class From(SQL): # from (interpolated)
     _source: Union[Table, "Query"] # only 1 FromClause is supported right now
 
-    def __serialize(self) -> List[Token]:
+    def _serialize_(self) -> List[Token]:
         return [Token("FROM", C.KEYWORD)] + self._table_or_query(self._source)
 
     def __init__(self, arg: Union[Table, "Query"]):
+        super().__init__()
         self._source = arg
 class Select(SQL):
     _nt1: Optional[Union[Struct, Value]]
     _nt2: Optional[SelectMode]
     _cols: List[Col] # this is provisional
 
-    def __serialize(self) -> List[Token]:
+    def _serialize_(self) -> List[Token]:
         return [Token("SELECT", C.KEYWORD)] + self._sep([col._serialize() for col in self._cols], COMMA)
 
     def __init__(self, cols: Union[List[Col], str]):
-        if isinstance(cols, List[Col]):
+        super().__init__()
+        if isinstance(cols, List):
             self._cols = cols
         elif isinstance(cols, str):
             self._cols = [Col(cols)]
 class With(SQL): # with (interpolated)
     _queries: List["Query"]
 
-    def __serialize(self) -> List[Token]:
-        return [Token("WITH", C.KEYWORD)] + self._sep([cte._serialize() for cte in self._ctes], COMMA)
+    def _serialize_(self) -> List[Token]:
+        return [Token("WITH", C.KEYWORD)] + self._sep([cte._serialize() for cte in self._queries], COMMA)
 
     def __init__(self, queries: List["Query"]):
+        super().__init__()
         for query in queries:
-            assert not (query._pre_alias is None and query._post_alias is None), "Each query in a with clause needs an alias"
+            assert not (query._pre_alias is None and query._post_alias is None and query._raw is None), "Each query in a with clause needs an alias"
             if query._post_alias:
                 query._pre_alias = query._post_alias
                 query._post_alias = None
         self._queries = queries
-# select:
-#     SELECT [ AS { STRUCT | VALUE } ] [{ ALL | DISTINCT }]
-#         { [ expression. ]* [ EXCEPT ( column_name [, ...] ) ]
-#             [ REPLACE ( expression [ AS ] column_name [, ...] ) ]
-#         | expression [ [ AS ] alias ] } [, ...]
-#     [ FROM from_clause[, ...] ]
-#     [ WHERE bool_expression ]
-#     [ GROUP BY { expression [, ...] | ROLLUP ( expression [, ...] ) } ]
-#     [ HAVING bool_expression ]
-#     [ QUALIFY bool_expression ]
-#     [ WINDOW window_clause ]
 # TODO document Col and Join CFG changes
 class Query(SQL): # query_expr
     _with: Optional[With]
@@ -189,9 +209,10 @@ class Query(SQL): # query_expr
     # syntactic sugar for changing or setting alias
     def AS(self, alias: str) -> None:
         self._post_alias = alias
+        return self
 
-    def __serialize(self) -> List[Token]:
-        query = self._chain(
+    def _serialize_(self) -> List[Token]:
+        query = self._chain([
             self._with,
             self._operation,
             self._from,
@@ -202,7 +223,7 @@ class Query(SQL): # query_expr
             self._qualify,
             self._order_by,
             self._limit
-        )
+        ])
         assert self._pre_alias is None or self._post_alias is None, "Cannot have 2 aliases for 1 query. It should be impossible to get this error without directly modifying private fields"
         if self._pre_alias:
             return [Token(self._pre_alias, C.IDENTIFIER), AS, LP] + query + [RP]
@@ -217,7 +238,7 @@ class Query(SQL): # query_expr
         WITH: Optional[Union[List["Query"], With]] = None,
         SELECT: Union[str, List[Col], Select] = "*",
         # these all technically belong to select
-        FROM: Optional[Union[str, "Query", From]] = None,
+        FROM: Optional[Union[Table, "Query", From]] = None,
         JOIN: Optional[Tuple[Union[Table, "Query"], Union[On, Using]]] = None, # will be supported with something like the signature below in a (near-)future release
         # JOIN: Optional[Union[str, Tuple[Union[str, "Query"], JoinCondition], Join]] = None,
         WHERE: Optional[Where] = None,
@@ -231,24 +252,25 @@ class Query(SQL): # query_expr
         AS: Optional[str] = None,
         # the rest of the join types go here to avoid cluttering the tooltip
     ):
+        super().__init__()
         # _with
         if WITH is None:
             self._with = None
-        elif isinstance(WITH, List["Query"]):
+        elif isinstance(WITH, List):
             self._with = With(WITH)
         elif isinstance(WITH, With):
             self._with = WITH
         
         # _operation (only select for now)
-        if isinstance(SELECT, str) or isinstance(SELECT, List[Col]):
+        if isinstance(SELECT, str) or isinstance(SELECT, List):
             self._operation = Select(SELECT)
         elif isinstance(SELECT, Select):
             self._operation = SELECT
 
         # _from
         if FROM is None:
-            self._from = From() # TODO should maybe make raw() smarter and then raise here
-        elif isinstance(FROM, str) or isinstance(FROM, Query):
+            self._from = From(Table.raw(""))
+        elif isinstance(FROM, Table) or isinstance(FROM, Query):
             self._from = From(FROM)
         elif isinstance(FROM, From):
             self._from = FROM
@@ -267,6 +289,7 @@ class Query(SQL): # query_expr
         self._window = WINDOW
 
         # aliases
+        self._pre_alias = None
         self._post_alias = AS
 
         # query args (raw only)
--- barbq/tests/foo_test.py
+++ barbq/tests/foo_test.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from barbq.query import Col, On, Query, Table
+from barbq.query import Col, On, Query, Table, Where
 class TestQuery(TestCase):
     def test_minimal_query(self):
         basic_query = Query(
@@ -8,18 +8,15 @@ class TestQuery(TestCase):
                 Col("specialty"),
                 Col("grannies.hobby"),
             ],
-            FROM=Table("acceptance-237317.kirby.grandmothers"),
+            FROM=Table("acceptance-237317.kirby.grandmothers", AS="grannies"),
         )
         print(basic_query.render())
-        expected = """
-SELECT
-    grannies.name,specialty,grannies.hobby
-FROM acceptance-237317.kirby.grandmothers"""
+        expected = """SELECT `grannies`.`name` , `specialty` , `grannies`.`hobby` FROM `acceptance-237317`.`kirby`.`grandmothers` AS `grannies`"""
         assert basic_query.render() == expected
 
     def test_omniquery(self):
         ref_scores = Table("acceptance-237317.kirby.ref_scores")
-        grandmothers = Table("acceptance-237317.kirby.grandmothers")
+        grandmothers = Table("acceptance-237317.kirby.grandmothers").AS("grannies")
         omniquery = Query(
             WITH=[
                 Query(
@@ -43,8 +40,8 @@ FROM acceptance-237317.kirby.grandmothers"""
                         Col("hilda_score"),
                         Col("mary_score"),
                     ],
-                    FROM=grandmothers.AS("grannies"),
-                    JOIN=(Table("summary_scores"), On.raw("summary_scores.name=grannies.name")),
+                    FROM=grandmothers,
+                    JOIN=(Table("summary_scores"), On.raw("ON summary_scores.name=grannies.name")),
                     AS="scored_grandmas",
                 ),
                 Query.raw(
@@ -56,11 +53,10 @@ FROM acceptance-237317.kirby.grandmothers"""
                 Col("name", AS="granny"),
                 Col.raw("relevance_score * mary_score AS score"),
             ],
-            FROM="combos",
-            WHERE="""
-                granny='mary'
-                -- for brands that care about similarity to mary, show how much they are interested in each of our grandmothers
-            """,
+            FROM=Table("combos"),
+            WHERE=Where.raw("""WHERE granny='mary' -- for brands that care about similarity to mary, show how much they are interested in each of our grandmothers"""),
         )
         print(omniquery.render())
-        assert False
+        expected = """WITH `scored_grandmas` AS ( WITH `summary_scores` AS ( SELECT `name` , CAST(hilda AS float64) + CAST(cookies AS float64) + CAST(knitting AS float64) AS hilda_score , CAST(mary AS float64) + CAST(bread as float64) + CAST(hockey AS float64) AS mary_score FROM `acceptance-237317`.`kirby`.`grandmothers` AS `grannies` ) SELECT `grannies`.`name` AS `name` , `hilda_score` , `mary_score` FROM `acceptance-237317`.`kirby`.`grandmothers` AS `grannies` JOIN `summary_scores` ON summary_scores.name=grannies.name ) , combos AS (SELECT * FROM scored_grandmas CROSS JOIN `acceptance-237317`.`kirby`.`ref_scores`) SELECT `brand` , `name` AS `granny` , relevance_score * mary_score AS score FROM `combos` WHERE granny='mary' -- for brands that care about similarity to mary, show how much they are interested in each of our grandmothers"""
+        print(expected)
+        assert omniquery.render() == expected
