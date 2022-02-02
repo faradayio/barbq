ba7b88f4793e9a483c8740804ba041601147efc3
Kirby Gordon <kirby.gordon@faraday.io>
2022-02-02 09:39:22 -0500

    new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='barbq',
-    version='0.0.4',
+    version='0.0.5',
     description='Python-native SQL dialect that renders to BigQuery',
     url='https://github.com/faradayio/barbq',
     author='Kirby Gordon',
