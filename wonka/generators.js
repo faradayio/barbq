797a5e8ac7dbc4757f2e7239e1bd472be9909cd1
Kirby Gordon <kirby.gordon@faraday.io>
2022-02-24 11:08:22 -0800

    delete wonka

--- wonka/generators.js
+++
@@ -1,6 +0,0 @@
-const { build_python_class } = require('./generators/python.js')
-const { build_ruby_class } = require('./generators/ruby.js')
-const { build_rust_class } = require('./generators/rust.js')
-const { build_typescript_class } = require('./generators/typescript.js')
-
-module.exports = { build_python_class, build_ruby_class, build_rust_class, build_typescript_class }
--- wonka/generators/rust.js
+++
@@ -1,8 +0,0 @@
-const build_rust_class = (name, root) => `
-class ${name}:
-    _data: ${build_type(root)}
-    def __init__(self, *args):
-        self._data = (${root.cogs.map(cog => `([arg for arg in args if matches_type(arg, ${no_quotes(build_type(cog))})] or [None])[0]`).join(', ')})
-`
-
-module.exports = { build_rust_class }
