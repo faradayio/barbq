d831189c9a33f64d531cf17875ad40a21ea21ffa
Kirby Gordon <kirby.gordon@faraday.io>
2022-03-09 09:18:29 -0800

    minor grammar changes

--- generator/builder.js
+++ generator/builder.js
@@ -1,12 +1,8 @@
 const { generate_classes } = require('../wonka/type_system_generator')
 const { Type, T, OPT, REP, ALL, ONE } = require('../wonka/grammar_builder')
 
-// types
-const query = Type('Query')
-
-// nonterminal tokens
 bigquery = [
-    T(query, )
+    
 ]
 
 generate_classes('./preamble.py', bigquery, './generated.py', 'python')
